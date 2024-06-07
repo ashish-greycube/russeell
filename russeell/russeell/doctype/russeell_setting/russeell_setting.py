e0760526b9c94cdad552c8e9ccff4e4a3221e480
Sakshi-Greycube <sakshi@greycube.in>
2024-06-07 11:07:02 +0530

    feat: validate russeell setting values

--- russeell/russeell/doctype/russeell_setting/russeell_setting.py
+++ russeell/russeell/doctype/russeell_setting/russeell_setting.py
@@ -1,9 +1,12 @@
 # Copyright (c) 2024, GreyCube Technologies and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe import _
 
 
 class RusseellSetting(Document):
-	pass
+	def validate(self):
+		if self.admin_fees_percentage <= 0 or self.sales_margin_percentage <= 0 or self.no_of_hours_in_a_month <= 0 or self.suggested_sales_markup_percentage <= 0:
+			frappe.throw(_("values cann't be 0 or negative"))
