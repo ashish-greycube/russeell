e0de84866e889d5720844fecbb54b69b48f422d3
ashish-greycube <mr.ashish.shah@gmail.com>
2024-07-08 19:27:49 +0530

    minor: dimension

--- russeell/russeell/doctype/business_unit_cd/test_business_unit_cd.py
+++ russeell/russeell/doctype/business_unit_cd/test_business_unit_cd.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestBusinessUnit(FrappeTestCase):
+class TestBusinessUnitCD(FrappeTestCase):
 	pass
--- russeell/russeell/doctype/city_cd/city_cd.py
+++ russeell/russeell/doctype/city_cd/city_cd.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class BusinessUnit(Document):
+class CityCD(Document):
 	pass
